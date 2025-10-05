579bb938b613f4bfaf85dc22057a08cb2d582f50
Rasel2276 <mdraselhossain2276@gmail.com>
2025-10-05 12:59:14 +0600

    up

--- first-project/resources/views/welcome.blade.php
+++ first-project/resources/views/welcome.blade.php
@@ -3,9 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Laravel</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
 </head>
 <body>
-    <h1>hello</h1>
+    <h1>Hello Laravel</h1>
 </body>
 </html>
